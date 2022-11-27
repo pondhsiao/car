034db9bdc73eb50925278e2d4a09c7e1aebece75
pondhsiao <98569846+pondhsiao@users.noreply.github.com>
2022-11-27 21:52:41 +0800

    依照距離變速

--- main.ts
+++ main.ts
@@ -4,6 +4,19 @@ basic.forever(function () {
         maqueen.motorStop(maqueen.Motors.M2)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) > 100) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        control.waitMicros(300000)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) > 50) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     } else {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 22)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 22)
